eee39f3dfb120d608ff851a347174b798ac81ce0
Tudor Tocan <tudor.tocan@gmail.com>
2022-04-13 10:14:34 +0100

    add shopping-list component

---
+++ src/components/shopping-list/ShoppingList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./shopping-list.css";
+
+const ShoppingList = () => {
+  return (
+    <div>
+      <h3>Current</h3>
+      <div className="shopping-list">
+        <div>Element 1</div>
+        <div>Element 1</div>
+        <div>Element 1</div>
+        <div>Element 1</div>
+        <div>Element 1</div>
+        <div>Element 1</div>
+      </div>
+    </div>
+  );
+};
+
+export default ShoppingList;
